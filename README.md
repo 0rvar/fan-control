59942ba0db38367ddccafc714c9f8aa72180e389
Orvar Segerström <orvarsegerstrom@gmail.com>
2025-01-18 13:11:49 +0100

    Working blinky!!

---
+++ README.md
@@ -0,0 +1,10 @@
+# Get up and running
+
+1. direnv allow
+2. cargo install espup
+3. espup install
+4. cargo build
+
+# Fixelifix
+
+- `espflash board-info` - Get information about the connected board
---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Devshell with all the dependencies needed to develop and build the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      # Boilerplate function for generating attributes for all systems
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ]
+          (
+            system:
+            (function (
+              import nixpkgs {
+                inherit system;
+              }
+            ))
+              system
+          );
+    in
+    {
+      packages = forAllSystems (
+        pkgs: system:
+        let
+          tools = with pkgs; [
+            espflash
+            ldproxy
+          ];
+          inputs = with pkgs; [
+            libiconv
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = tools ++ inputs;
+            shellHook = ''
+              export LIBCLANG_PATH="$HOME/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-18.1.2_20240912/esp-clang/lib"
+              export PATH="$HOME/.rustup/toolchains/esp/xtensa-esp-elf/esp-14.2.0_20240906/xtensa-esp-elf/bin:$PATH"
+            '';
+          };
+        }
+      );
+    };
+}
